1c55af49c03c4afbd158a1cad3e8b4489e20efeb
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-03-23 17:28:29 -0400

    Add files via upload

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -44,7 +44,6 @@ public class SudokuSolver {
 	}
 	
 	public void availabilityCalc() {
-		Random rand = new Random();
 		ArrayList<HashSet<Integer>> availabilityLists = new ArrayList<HashSet<Integer>>(81);
 		for (int i = 0; i < zeroCount; i++) {
 			availabilityLists.add(i, new HashSet<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
@@ -61,34 +60,185 @@ public class SudokuSolver {
 						}
 					}
 					if (i <= 2 && j <= 2) {
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 						
 					}
 					else if ((i <= 2) && (j >= 3 && j <= 5)) {
-						
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
-					else if ((i <= 2) && (j >= 6 && j <= 8)) {
-						
+					else if ((i <= 2) && (j >= 5 && j <= 7)) {
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 5; c <= 7; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else if ((i >= 3 && i <= 5) && j <= 2) {
-						
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else if ((i >= 3 && i <= 5) && (j >= 3 && j <= 5)) {
-						
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else if ((i >= 3 && i <= 5) && (j >= 6 && j <= 8)) {
-						
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 6; c <= 8; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else if ((i >= 6 && i <= 8) && j <= 2) {
-						
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else if ((i >= 6 && i <= 8) && (j >= 3 && j <= 5)) {
-						
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					else {
-						
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 6; c <= 8; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).remove(puzzle[r][c]);
+							}
+						}
 					}
 					count++;
 				}
+			}
+		}
+		System.out.println();
+		System.out.println(availabilityLists);
+		System.out.println(availabilityLists.size());
+	}
+	
+	public void availabilityCalc3D() {
+		ArrayList<ArrayList<ArrayList<Integer>>> availabilityLists = new ArrayList<ArrayList<ArrayList<Integer>>>(81);
+		
+		for (int i = 0, count = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				Integer[] index = {i, j};
+				if (puzzle[i][j] == 0) {
+					availabilityLists.add(new ArrayList<ArrayList<Integer>>());
+					availabilityLists.get(count).add(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
+					availabilityLists.get(count).add(new ArrayList<Integer>(Arrays.asList(i, j)));
+	
+					for (int k = 0; k < 9; k++) {
+						if (puzzle[k][j] != 0) {
+							availabilityLists.get(count).get(0).remove(puzzle[k][j]);
+						}
+						if (puzzle[i][k] != 0) {
+							availabilityLists.get(count).get(0).remove(puzzle[i][k]);
+						}
+					}
+					if (i <= 2 && j <= 2) {
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+						
+					}
+					else if ((i <= 2) && (j >= 3 && j <= 5)) {
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i <= 2) && (j >= 5 && j <= 7)) {
+						for (int r = 0; r <= 2; r++) {
+							for (int c = 5; c <= 7; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i >= 3 && i <= 5) && j <= 2) {
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i >= 3 && i <= 5) && (j >= 3 && j <= 5)) {
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i >= 3 && i <= 5) && (j >= 6 && j <= 8)) {
+						for (int r = 3; r <= 5; r++) {
+							for (int c = 6; c <= 8; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i >= 6 && i <= 8) && j <= 2) {
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 0; c <= 2; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else if ((i >= 6 && i <= 8) && (j >= 3 && j <= 5)) {
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 3; c <= 5; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+					else {
+						for (int r = 6; r <= 8; r++) {
+							for (int c = 6; c <= 8; c++) {
+								if (puzzle[r][c] != 0)
+									availabilityLists.get(count).get(0).remove(puzzle[r][c]);
+							}
+						}
+					}
+				}
+				else {
+					availabilityLists.add(new ArrayList<ArrayList<Integer>>());
+					availabilityLists.get(count).add(new ArrayList<Integer>());
+				}
+				count++;
 			}
 		}
 		System.out.println();
